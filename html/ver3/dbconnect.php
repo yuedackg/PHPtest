92a3b31f44dc82d78adcda3f14fe3f0e5f1034c0
Yoshihiro UEDA <y.ueda@ckg.ac.jp>
2023-06-11 21:07:51 +0900

    PHP初級#1クリア

---
+++ html/ver3/dbconnect.php
@@ -0,0 +1,36 @@
+<?php
+
+require('./env.php');
+
+function connect()
+{
+	$host = DB_HOST;
+	$db = DB_NAME;
+	$user = DB_USER;
+	$pass = DB_PASS;
+
+	$dsn = "mysql:host=$host;dbname=$db;charset=utf8mb4";
+	echo $dsn ;
+echo "<br>";
+
+	$pdo =null;
+	try {
+		$pdo = new PDO($dsn, $user, $pass, [
+			PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+			PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+		]);
+		// var_dump($pdo->errorInfo());
+
+		echo "成功です";
+	} catch (PDOException $e) {
+		echo "接続失敗です" . $e->getMessage();
+		// var_dump($pdo->errorInfo());
+
+		exit();
+	}
+
+
+}
+
+echo "hello";
+echo connect();
---
+++ html/ver3/env.php
@@ -0,0 +1,8 @@
+<?php
+
+// hostの指定は、dockler-compose.yamlで指定されたサービスの名前にする
+define( 'DB_HOST', 'mysql');	
+define( 'DB_NAME', 'example_schema');
+define( 'DB_USER', 'example_user');
+define( 'DB_PASS', 'example_pass');
+
